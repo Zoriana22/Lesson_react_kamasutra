eb2ccfa6bb43e5fa1184d1f1253a30b47886c986
PaninaAY <PaninaAY@bk.ru>
2023-12-23 18:21:35 +0200

    mistakes

--- app/src/components/Users/Users.jsx
+++ app/src/components/Users/Users.jsx
@@ -34,7 +34,7 @@ let Users = (props) => {
                             {u.followed ?
                                 <button onClick={() => {
 
-                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/unfollow${u.id}`, {
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/unfollow/${u.id}`, {
                                         withCredentials: true,
                                         headers: { 'API-KEY': 'bb0cb20a-6c00-42ea-96fb-f598fa987298' },
                                     })
@@ -46,7 +46,7 @@ let Users = (props) => {
                                 }}>Unfollow</button >
                                 : <button onClick={() => {
 
-                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow${u.id}`, {}, {
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
                                         withCredentials: true,
                                         headers: { 'API-KEY': 'bb0cb20a-6c00-42ea-96fb-f598fa987298' },
                                     })
