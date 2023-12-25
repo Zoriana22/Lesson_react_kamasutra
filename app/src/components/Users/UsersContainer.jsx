21fb40ee7647ffad15a1613747738f3a423309b7
PaninaAY <PaninaAY@bk.ru>
2023-12-25 20:31:39 +0200

    compose does not work

--- app/src/components/Users/UsersContainer.jsx
+++ app/src/components/Users/UsersContainer.jsx
@@ -3,8 +3,25 @@ import { connect } from 'react-redux';
 import { follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers } from '../../redux/users-reducer';
 import Users from './Users';
 import Preloader from './Preloader';
-import { compose } from 'redux';
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { withAuthRedirect } from '../../hoc/withAuthRedirect';
+import { compose } from 'redux';
+
+// wrapper to use react router's v6 hooks in class component(to use HOC pattern, like in router v5)
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return (
+      <Component
+        {...props}
+        router={{ location, navigate, params }}
+      />
+    );
+  }
+  return ComponentWithRouterProp;
+}
 
 class UsersContainer extends React.Component {
   constructor(props) {
@@ -38,6 +55,8 @@ class UsersContainer extends React.Component {
   };
 }
 
+let AuthRedirectComponent = withAuthRedirect(UsersContainer);
+
 let mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
@@ -49,38 +68,16 @@ let mapStateToProps = (state) => {
   }
 }
 
-// let mapDispatchToProps = (dispatch) => {
-//   return {
-//     follow: (userId) => {
-//       dispatch(followAC(userId))
-//     },
-//     unfollow: (userId) => {
-//       dispatch(unfollowAC(userId))
-//     },
-//     setUsers: (users) => {
-//       dispatch(setUsersAC(users))
-//     },
-//     setCurrentPage: (pageNumber) => {
-//       dispatch(setCurrentPageAC(pageNumber))
-//     },
-//     setTotalUsersCount: (totalCount) => {
-//       dispatch(setTotalUsersCountAC(totalCount))
-//     },
-//     toggleIsFetching: (isFetching) => {
-//       dispatch(toggleIsFetchingAC(isFetching))
-//     },
-//   }
-// }
 
-export default connect(mapStateToProps, {
-  follow,
-  unfollow,
-  setCurrentPage,
-  toggleFollowingProgress,
-  getUsers,
-})(UsersContainer);
+// export default connect(mapStateToProps, {
+//   follow,
+//   unfollow,
+//   setCurrentPage,
+//   toggleFollowingProgress,
+//   getUsers,
+// })(UsersContainer);
 
-// export default compose(
-//   connect(mapStateToProps, { follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers, }),
-//   withAuthRedirect,
-// )(UsersContainer)
+export default compose(
+  connect(mapStateToProps, { follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers, }),
+  withRouter(AuthRedirectComponent),
+)(UsersContainer)
